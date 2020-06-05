55f2d99bc29f45848340bb5f13113fc211bcb3c7
Diego <diegofernandezhuanquitripay@gmail.com>
2020-06-05 16:58:56 -0400

    agregue el controlador alumnos y modifique vistas varias

---
+++ Controllers/AlumnoController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ITF.Controllers
+{
+    public class AlumnoController : Controller
+    {
+        // GET: Alumno
+        public ActionResult Home()
+        {
+            return View();
+        }
+
+        public ActionResult MiProgresion()
+        {
+            return View();
+        }
+
+        public ActionResult Tienda()
+        {
+            return View();
+        }
+
+        public ActionResult Examenes()
+        {
+            return View();
+        }
+
+        public ActionResult Eventos()
+        {
+            return View();
+        }
+
+        public ActionResult Ahorros()
+        {
+            return View();
+        }
+    }
+}
